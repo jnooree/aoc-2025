8eb1e7e7d24c6f0426aa7dd283d4d9f8265c49da
Nuri Jung <jnooree@snu.ac.kr>
2025-12-03 17:40:30 +0900

    feat(day03): solve part 2

--- src/aoc2025/solutions/day03.py
+++ src/aoc2025/solutions/day03.py
@@ -3,12 +3,11 @@ from typer import FileText
 
 
 def main(data: FileText):
-    nums = np.genfromtxt(data, dtype=np.int8, delimiter=1, invalid_raise=True)
+    nums = np.genfromtxt(data, dtype=np.int64, delimiter=1, invalid_raise=True)
     n = nums.shape[1]
+    indices = np.arange(n)
 
     first = np.argmax(nums[:, :-1], axis=1, keepdims=True)
-
-    indices = np.arange(n)
     masked_nums = np.where(indices > first, nums, -1)
     second = np.argmax(masked_nums, axis=1, keepdims=True)
 
@@ -16,3 +15,16 @@ def main(data: FileText):
         nums, first, axis=1
     ) + np.take_along_axis(nums, second, axis=1)
     print(np.sum(joltages))
+
+    total = 0
+    masked_nums = nums
+    for i in reversed(range(12)):
+        argdigit = np.argmax(
+            masked_nums[:, : -i or None],
+            axis=1,
+            keepdims=True,
+        )
+        total = total * 10 + np.take_along_axis(masked_nums, argdigit, axis=1)
+        masked_nums = np.where(indices > argdigit, masked_nums, -1)
+
+    print(np.sum(total))
