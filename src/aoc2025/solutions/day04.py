3ecfe3a82fd342566f143394af250fd2f95ab34b
Nuri Jung <jnooree@snu.ac.kr>
2025-12-05 13:39:26 +0900

    feat(day04): solve part 2

--- src/aoc2025/solutions/day04.py
+++ src/aoc2025/solutions/day04.py
@@ -1,7 +1,13 @@
 import numpy as np
 import torch
+from torch import Tensor
 from torch.nn import functional as F
 from typer import FileText
+
+
+def _select_accessible(grid: Tensor, kernel: Tensor):
+    nbrs = F.conv2d(grid[None, None], kernel, padding="same").squeeze()
+    return (nbrs < 4) & (grid == 1)
 
 
 def main(data: FileText):
@@ -19,6 +25,13 @@ def main(data: FileText):
 
     kernel = grid.new_ones(1, 1, 3, 3)
     kernel[..., 1, 1] = 0
-    nbrs = F.conv2d(grid[None, None], kernel, padding="same").squeeze()
 
-    print(torch.sum((nbrs < 4) & (grid == 1)).item())
+    accessible = _select_accessible(grid, kernel)
+    count = torch.sum(accessible)
+    print(count.item())
+
+    while torch.any(accessible):
+        grid -= accessible.to(grid.dtype)
+        accessible = _select_accessible(grid, kernel)
+        count += torch.sum(accessible)
+    print(count.item())
