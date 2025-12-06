cef7f9f5e6a797a21845cc54b84ab17b47f9f6dc
Nuri Jung <jnooree@snu.ac.kr>
2025-12-06 18:59:26 +0900

    feat(day06): solve part 2

--- src/aoc2025/solutions/day06.py
+++ src/aoc2025/solutions/day06.py
@@ -10,3 +10,20 @@ def main(data: FileText):
     mul = np.prod(nums[:, ops == "*"], axis=0)
     add = np.sum(nums[:, ops == "+"], axis=0)
     print(np.sum(mul) + np.sum(add))
+
+    digits = np.array(list(map(list, lines[:-1])), dtype=np.str_).T
+
+    (num_end,) = np.where(np.all(digits == " ", axis=1))
+    num_end = np.append(num_end, digits.shape[0])
+    num_begin = np.insert(num_end[:-1] + 1, 0, 0)
+
+    nums = np.array(
+        ["".join(line) if np.any(line != " ") else -1 for line in digits],
+        dtype=np.int64,
+    )
+
+    total = 0
+    for b, e, o in zip(num_begin, num_end, ops):
+        oper = np.prod if o == "*" else np.sum
+        total += oper(nums[b:e])
+    print(total)
