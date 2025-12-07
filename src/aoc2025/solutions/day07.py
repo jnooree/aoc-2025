a176ec8831436ba1083c81d9b30d67f266bb9503
Nuri Jung <jnooree@snu.ac.kr>
2025-12-07 22:14:16 +0900

    feat(day07): solve part 2

--- src/aoc2025/solutions/day07.py
+++ src/aoc2025/solutions/day07.py
@@ -18,3 +18,19 @@ def main(data: FileText):
             | np.append(splitted[1:], False)
         )
     print(nsplit)
+
+    prev_ntraj = (manifold[0] == "S").astype(np.int64)
+    for row in manifold[1:]:
+        splitter = row == "^"
+        splitter_idxs = np.argwhere(splitter).flatten()
+        if splitter_idxs.size == 0:
+            continue
+
+        next_ntraj = np.zeros_like(prev_ntraj)
+        next_ntraj[~splitter] = prev_ntraj[~splitter]
+        next_ntraj[splitter_idxs - 1] += prev_ntraj[splitter]
+        next_ntraj[splitter_idxs + 1] += prev_ntraj[splitter]
+
+        prev_ntraj = next_ntraj
+
+    print(np.sum(prev_ntraj))
